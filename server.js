9262363d347ff1843db688a4a017df6365537361
Millie Walsh <millie@primeacademy.io>
2017-04-27 16:05:34 -0500

    clean up comments

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 
-// http://localhost:3003/testParams/45
+// url params
+// These requests are examples of how to hit the
+// testParams route below:
+//http://localhost:3003/testParams/45
 //http://localhost:3003/testQuery/bob
-
 app.get('/testParams/:id', function(req, res) {
   console.log('in get testParams');
   console.log('req.params ->', req.params.id);
   res.sendStatus(200);
 });
+
 // query string
-//http://localhost:3003/testQuery?name=bob
+// These requests are expamples of how to hit testQuery
+// route below:
 //http://localhost:3003/testQuery?name=bob
 //http://localhost:3003/test?id=45
-
 app.get('/testQuery', function(req, res) {
   console.log('in get testQuery');
   console.log('req.query ->', req.query);
